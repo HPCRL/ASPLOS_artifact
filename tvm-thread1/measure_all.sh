96bf0ec9381c84a450f7a3b2900b07d458ee8af8
Terex12 <sz.yufanxu@gmail.com>
2020-08-28 10:17:38 -0600

    add README and tvm experiment

---
+++ tvm-thread1/measure_all.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+FPATH=...
+
+pwd
+cd $FPATH/yolo/
+pwd
+bash modi-llvm.sh
+sleep 5
+bash generate_so.script
+sleep 5
+bash taskrun.sh
+sleep 5
+echo "[+] yolo DONE M~"
+cd ..
+
+pwd
+cd $FPATH/resnet/
+pwd
+bash modi-llvm.sh
+sleep 5
+bash generate_so.script
+sleep 5
+bash taskrun.sh
+echo "[+] resnet DONE M~"
+
+sleep 5
+cd ..
+pwd
+cd $FPATH/mob/
+bash modi-llvm.sh
+sleep 5
+bash generate_so.script
+sleep 5
+bash taskrun.sh
+echo "[+] mob DONE M~"
+
---
+++ tvm-thread1/yolo/rmv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm *.txt
+rm *.ll
+rm *.so
+rm *.log
